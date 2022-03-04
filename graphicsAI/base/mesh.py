229ebd26b59f53711cfd39dccf55289adc8719f2
LadDeep <deeplad2000@gmail.com>
2022-03-04 14:26:22 +0530

    implementing mesh class

--- graphicsAI/base/mesh.py
+++ graphicsAI/base/mesh.py
@@ -1,16 +1,16 @@
-from core.object3D import Object3D 
 from OpenGL.GL import *
+from graphicsAI.base.object3D import Object3D
 
 class Mesh(Object3D):
     def __init__(self, geometry, material): 
         super().__init__()
         self.geometry = geometry 
         self.material = material
-        self.visible = True
+        self.isVisible = True
        
         self.vaoRef = glGenVertexArrays(1) 
         glBindVertexArray(self.vaoRef) 
-        for variableName, attributeObject in geometry.attributes.items(): 
-            attributeObject.associateVariable(material.programRef, variableName) 
+        for varName, attrObject in geometry.attributes.items(): 
+            attrObject.associateVariable(material.programRef, varName) 
         
         glBindVertexArray(0)
